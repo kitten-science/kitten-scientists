kitten-science/kitten-scientists

commit 8d5135c6df0231da12d900be2a82b8844eb3427f

issue #49: allow leviathans to trade even when capped
Also users can modify options.auto.trade.items[name].allowuncapped in
debug console if they need. Possibly a configuration setting should be
allowed, but I honestly don't think one is necessary here.

This patch also modifies some of the default race settings, since those
races primary best trade time was different. I chose to set races same in
each season with good output to be enabled in all seasons. Ofcourse users
can customize that.

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

kitten-scientists.js
@@ -90,28 +90,28 @@ var options = {
         },
         trade: {
             enabled: true, trigger: 0.95, items: {
-                dragons: {enabled: false, trigger: 0.80, require: 'titanium',
-                          summer: true, autumn: true, winter: false, spring: true},
+                dragons: {enabled: false, trigger: 0.80, require: 'titanium', allowcapped: false,
+                          summer: true, autumn: true, winter: true, spring: true},
 
-                zebras: {enabled: true, trigger: 0.80, require: false,
-                         summer: true, autumn: false, winter: false, spring: false},
+                zebras: {enabled: true, trigger: 0.80, require: false, allowcapped: false,
+                         summer: true, autumn: true, winter: true, spring: true},
 
-                lizards: {trigger: 0.80, require: 'minerals',
+                lizards: {enabled: false, trigger: 0.80, require: 'minerals', allowcapped: false,
                           summer: true, autumn: false, winter: false, spring: false},
 
-                sharks: {enabled: false, trigger: 0.80, require: 'iron',
+                sharks: {enabled: false, trigger: 0.80, require: 'iron', allowcapped: false,
                          summer: false, autumn: false, winter: true, spring: false},
 
-                griffins: {enabled: false, trigger: 0.80, require: 'wood',
-                           summer: true, autumn: false, winter: false, spring: false},
+                griffins: {enabled: false, trigger: 0.80, require: 'wood', allowcapped: false,
+                           summer: false, autumn: true, winter: false, spring: false},
 
-                nagas: {enabled: false, trigger: 0.80, require: false,
+                nagas: {enabled: false, trigger: 0.80, require: false, allowcapped: false,
                         summer: false, autumn: false, winter: false, spring: true},
 
-                spiders: {enabled: false, trigger: 0.80, require: false,
-                          summer: false, autumn: true, winter: false, spring: true},
+                spiders: {enabled: false, trigger: 0.80, require: false, allowcapped: false,
+                          summer: false, autumn: true, winter: false, spring: false},
 
-                leviathans: {enabled: false, trigger: 0.80, require: 'unobtainium',
+                leviathans: {enabled: false, trigger: 0.80, require: 'unobtainium', allowcapped: true,
                              summer: true, autumn: true, winter: true, spring: true},
             }
         },
@@ -534,7 +534,7 @@ TradeManager.prototype = {
             amount = (-1 === amount || total < amount) ? total : amount;
         }
 
-        if (race === null) return Math.floor(amount);
+        if (race === null || options.auto.trade.items[name].allowuncapped) return Math.floor(amount);
 
         // Loop through the items obtained by the race, and determine
         // which good has the most space left. Once we've determined this,

commit 2073a1d07f7ff744555c8cc5eda6ed78460616dc

trade: fix issue #61 leviathans trade
This patch fixes a typo of the use of allowuncapped instead of
allowcapped. This should (properly) fix leviathan trade.

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>
Fixes: 8d5135c ("issue #49: allow leviathans to trade even when capped")

kitten-scientists.js
@@ -583,7 +583,7 @@ TradeManager.prototype = {
             amount = (amount === undefined || total < amount) ? total : amount;
         }
 
-        if (race === null || options.auto.trade.items[name].allowuncapped) return Math.floor(amount);
+        if (race === null || options.auto.trade.items[name].allowcapped) return Math.floor(amount);
 
         // Loop through the items obtained by the race, and determine
         // which good has the most space left. Once we've determined this,