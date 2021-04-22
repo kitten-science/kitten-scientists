31be966c0ebecf2f2457d925761ae30c014fc2a2
Oliver Salzburg <oliver.salzburg@gmail.com>
2021-04-22 22:14:35 +0200

    feat: Options in TS

--- packages/userscript/source/UserScript.ts
+++ packages/userscript/source/UserScript.ts
@@ -1,13 +1,24 @@
+import JQuery from "jquery";
+import i18nData from "./i18n/i18nData.json";
+import { Options } from "./Options";
 import { isNil, Maybe, mustExist } from "./tools/Maybe";
 import { sleep } from "./tools/Sleep";
-import i18nData from "./i18n/i18nData.json";
 
 declare global {
   const unsafeWindow: Window;
-  interface Window { gamePage?: Maybe<GamePage>; $I?: Maybe<I18nEngine>; }
+  interface Window {
+    gamePage?: Maybe<GamePage>;
+    $: JQuery;
+    $I?: Maybe<I18nEngine>;
+  }
 }
 
-export interface GamePage { }
+export interface GamePage {
+  console: {
+    maxMessages: number;
+  };
+  msg: (...args: Array<string>) => { span: HTMLElement };
+}
 export type I18nEngine = (key: string) => string;
 
 export type SupportedLanguages = keyof typeof i18nData;
@@ -19,8 +30,13 @@ export class UserScript {
   private _language: SupportedLanguages;
 
   private readonly _i18nData: typeof i18nData;
+  private _options: Options;
 
-  constructor(gamePage: GamePage, i18nEngine: I18nEngine, language: SupportedLanguages = DefaultLanguage) {
+  constructor(
+    gamePage: GamePage,
+    i18nEngine: I18nEngine,
+    language: SupportedLanguages = DefaultLanguage
+  ) {
     console.info("Kitten Scientists constructed.");
 
     this._gamePage = gamePage;
@@ -32,23 +48,37 @@ export class UserScript {
 
   async run(): Promise<void> {
     if (this._language in this._i18nData === false) {
-      console.warn(`Requested language '${this._language}' is not available. Falling back to '${DefaultLanguage}'.`);
+      console.warn(
+        `Requested language '${this._language}' is not available. Falling back to '${DefaultLanguage}'.`
+      );
       this._language = DefaultLanguage;
     }
+
+    // Increase messages displayed in log
+    this._gamePage.console.maxMessages = 1000;
   }
 
-  private _i18n(key: keyof typeof i18nData[SupportedLanguages], args: Array<number | string>): string {
+  /**
+   * Retrieve an internationalized string literal.
+   * @param key The key to retrieve from the translation table.
+   * @param args Variable arguments to render into the string.
+   * @returns The translated string.
+   */
+  private _i18n(
+    key: keyof typeof i18nData[SupportedLanguages],
+    args: Array<number | string>
+  ): string {
     if (key.startsWith("$")) {
       return this._i18nEngine(key.slice(1));
     }
     let value = this._i18nData[this._language][key];
-    if (typeof value === 'undefined') {
+    if (typeof value === "undefined") {
       value = i18nData[DefaultLanguage][key];
       if (!value) {
-        console.warn(`i18n key '${key}' not found in default language.`)
-        return '$' + key;
+        console.warn(`i18n key '${key}' not found in default language.`);
+        return "$" + key;
       }
-      console.warn(`i18n key '${key}' not found in selected language.`)
+      console.warn(`i18n key '${key}' not found in selected language.`);
     }
     if (args) {
       for (let argIndex = 0; argIndex < args.length; ++argIndex) {
@@ -56,6 +86,25 @@ export class UserScript {
       }
     }
     return value;
+  }
+
+  private _printOutput(...args: Array<string>): void {
+    if (this._options.auto.filter.enabled) {
+      for (var filt in this._options.auto.filter.items) {
+        var filter = this._options.auto.filter.items[filt];
+        if (filter.enabled && filter.variant === args[1]) {
+          return;
+        }
+      }
+    }
+    var color = args.pop();
+    args[1] = args[1] || "ks-default";
+
+    // update the color of the message immediately after adding
+    var msg = this._gamePage.msg.apply(this._gamePage, args);
+    $(msg.span).css("color", color);
+
+    if (this._options.debug && console) console.log(args);
   }
 
   static async waitForGame(timeout: number = 30000): Promise<void> {
@@ -74,7 +123,11 @@ export class UserScript {
   }
 
   static async getDefaultInstance(): Promise<UserScript> {
-    return new UserScript(mustExist(unsafeWindow.gamePage), mustExist(unsafeWindow.$I), localStorage['com.nuclearunicorn.kittengame.language']);
+    return new UserScript(
+      mustExist(unsafeWindow.gamePage),
+      mustExist(unsafeWindow.$I),
+      localStorage["com.nuclearunicorn.kittengame.language"]
+    );
   }
 
   private static _isGameLoaded(): boolean {
