bcb22d9e017fd9abbbaca378fbdcea40523c9341
Oliver Salzburg <oliver.salzburg@gmail.com>
2021-04-24 17:37:39 +0200

    fix: Sourcemaps

--- packages/userscript/webpack.config.inject.js
+++ packages/userscript/webpack.config.inject.js
@@ -3,14 +3,12 @@ const dev = process.env.NODE_ENV === 'development';
 const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
 
 module.exports = {
-  mode: dev ? 'development' : 'production',
+  mode: 'development',
+  devtool:"inline-source-map",
   entry: path.resolve(__dirname, 'output', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'output'),
     filename: 'kitten-scientists.inject.js'
-  },
-  optimization: {
-    minimize: false
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist')
