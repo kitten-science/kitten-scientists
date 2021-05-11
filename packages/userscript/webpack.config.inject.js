0195b6ce50f15786ec75cb1ea049fc1a41e93f48
Oliver Salzburg <oliver.salzburg@gmail.com>
2021-05-11 15:21:36 +0200

    feat: Build with ts-loader

--- packages/userscript/webpack.config.inject.js
+++ packages/userscript/webpack.config.inject.js
@@ -1,19 +1,27 @@
-const path = require('path');
-const dev = process.env.NODE_ENV === 'development';
-const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+const PnpWebpackPlugin = require("pnp-webpack-plugin");
 
 module.exports = {
   mode: 'development',
-  devtool:"inline-source-map",
-  entry: path.resolve(__dirname, 'output', 'index.js'),
+  devtool: "inline-source-map",
+  entry: path.resolve(__dirname, 'source', 'index.ts'),
   output: {
     path: path.resolve(__dirname, 'output'),
     filename: 'kitten-scientists.inject.js'
   },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist')
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: require.resolve("ts-loader"),
+        options: PnpWebpackPlugin.tsLoaderOptions(),
+      }
+    ]
   },
   resolve: {
+    extensions: [".ts", ".js"],
     plugins: [
       PnpWebpackPlugin,
     ],
