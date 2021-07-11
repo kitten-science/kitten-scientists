2c78109c5e587fd90fe4e31c81aaed36af3da3f3
Oliver Salzburg <oliver.salzburg@gmail.com>
2021-07-11 20:39:07 +0200

    chore: Set up working build scripts for userscript

--- packages/userscript/webpack.config.userscript.js
+++ packages/userscript/webpack.config.userscript.js
@@ -1,32 +1,32 @@
-const path = require('path');
-const WebpackUserscript = require('webpack-userscript');
-const dev = process.env.NODE_ENV === 'development';
-const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+const WebpackUserscript = require("webpack-userscript");
+const dev = process.env.NODE_ENV === "development";
+const PnpWebpackPlugin = require("pnp-webpack-plugin");
 
 module.exports = {
-  mode: dev ? 'development' : 'production',
-  entry: path.resolve(__dirname, 'output', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'output'),
-    filename: 'kitten-scientists.user.js'
-  },
-  optimization: {
-    minimize: false
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist')
-  },
-  plugins: [
-    new WebpackUserscript()
-  ],
-  resolve: {
-    plugins: [
-      PnpWebpackPlugin,
+  entry: path.resolve(__dirname, "source", "index.ts"),
+  mode: dev ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: require.resolve("ts-loader"),
+        options: PnpWebpackPlugin.tsLoaderOptions(),
+      },
     ],
+  },
+  output: {
+    path: path.resolve(__dirname, "output"),
+    filename: "kitten-scientists.user.js",
+  },
+  plugins: [new WebpackUserscript()],
+  resolve: {
+    extensions: [".ts", ".js"],
+    plugins: [PnpWebpackPlugin],
   },
   resolveLoader: {
-    plugins: [
-      PnpWebpackPlugin.moduleLoader(module),
-    ],
+    plugins: [PnpWebpackPlugin.moduleLoader(module)],
   },
-}
+};
