7b1c0dfa148ef1f9112a83c03f85e4a40f1d21a4
Oliver Salzburg <oliver.salzburg@gmail.com>
2021-07-12 00:06:44 +0200

    fix: Drop obsolete options

--- packages/userscript/source/options/FilterSettings.ts
+++ packages/userscript/source/options/FilterSettings.ts
@@ -1,5 +1,24 @@
-import { FilterItemVariant } from "./OptionsLegacy";
 import { SettingsSection } from "./SettingsSection";
+
+export enum FilterItemVariant {
+  Build = "ks-activity type_ks-build",
+  Craft = "ks-activity type_ks-craft",
+  Upgrade = "ks-activity type_ks-upgrade",
+  Research = "ks-activity type_ks-research",
+  Trade = "ks-activity type_ks-trade",
+  Hunt = "ks-activity type_ks-hunt",
+  Praise = "ks-activity type_ks-praise",
+  Adore = "ks-activity type_ks-adore",
+  Transcend = "ks-activity type_ks-transcend",
+  Faith = "ks-activity type_ks-faith",
+  Accelerate = "ks-activity type_ks-accelerate",
+  TimeSkip = "ks-activity type_ks-timeSkip",
+  Festival = "ks-activity type_ks-festival",
+  Star = "ks-activity type_ks-star",
+  Distribute = "ks-activity type_ks-distribute",
+  Promote = "ks-activity type_ks-promote",
+  Misc = "ks-activity",
+}
 
 export type FilterItem =
   | "accelerateFilter"
