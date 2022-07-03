1c3fced7f2a1b88d5369e00cf360c409995879cd
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-07-03 17:54:26 +0200

    fix(ui): Unable to expand unlock settings

--- packages/userscript/source/ui/UnlockingSettingsUi.ts
+++ packages/userscript/source/ui/UnlockingSettingsUi.ts
@@ -20,39 +20,15 @@ export class UnlockingSettingsUi extends SettingsSectionUi<UnlockingSettings> {
     this._options = options;
 
     const toggleName = "upgrade";
-
-    const itext = ucfirst(this._host.i18n("ui.upgrade"));
-
-    // Our main element is a list item.
-    const element = this._getSettingsPanel(toggleName, itext);
-
-    this._options.$enabled = element.checkbox;
-
-    element.checkbox.on("change", () => {
-      if (element.checkbox.is(":checked") && this._options.enabled === false) {
-        this._host.updateOptions(() => (this._options.enabled = true));
-        this._host.imessage("status.auto.enable", [itext]);
-      } else if (!element.checkbox.is(":checked") && this._options.enabled === true) {
-        this._host.updateOptions(() => (this._options.enabled = false));
-        this._host.imessage("status.auto.disable", [itext]);
-      }
-    });
+    const label = ucfirst(this._host.i18n("ui.upgrade"));
 
     // Create build items.
     // We create these in a list that is displayed when the user clicks the "items" button.
     const list = this._getOptionList(toggleName);
 
-    element.items.on("click", () => {
-      list.toggle();
-
-      this._itemsExpanded = !this._itemsExpanded;
-
-      element.items.text(this._itemsExpanded ? "-" : "+");
-      element.items.prop(
-        "title",
-        this._itemsExpanded ? this._host.i18n("ui.itemsHide") : this._host.i18n("ui.itemsShow")
-      );
-    });
+    // Our main element is a list item.
+    const element = this._getSettingsPanel(toggleName, label, this._options, list);
+    this._options.$enabled = element.checkbox;
 
     // Set up the more complex policies options.
     const policiesButton = this._getOption(
