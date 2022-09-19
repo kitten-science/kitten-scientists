72b080d29ed7654ce54f476dd31574ffad9c3ebd
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-09-19 19:13:13 +0200

    docs: Fix missing JSDoc

--- packages/userscript/source/tools/Log.ts
+++ packages/userscript/source/tools/Log.ts
@@ -1,16 +1,44 @@
-// Console interaction helpers.
+/**
+ * Print a debugging message to the console.
+ *
+ * @param {...any} args Arguments to pass to `console.debug`.
+ */
 export function cdebug(...args: Array<unknown>): void {
   console.debug("👩‍🔬", ...args);
 }
+
+/**
+ * Print an informational message to the console.
+ *
+ * @param {...any} args Arguments to pass to `console.info`.
+ */
 export function cinfo(...args: Array<unknown>): void {
   console.info("👩‍🔬", ...args);
 }
+
+/**
+ * Print a message to the console.
+ *
+ * @param {...any} args Arguments to pass to `console.log`.
+ */
 export function clog(...args: Array<unknown>): void {
   console.log("👩‍🔬", ...args);
 }
+
+/**
+ * Print a warning to the console.
+ *
+ * @param {...any} args Arguments to pass to `console.warn`.
+ */
 export function cwarn(...args: Array<unknown>): void {
   console.warn("👩‍🔬", ...args);
 }
+
+/**
+ * Print an error to the console.
+ *
+ * @param {...any} args Arguments to pass to `console.error`.
+ */
 export function cerror(...args: Array<unknown>): void {
   console.error("👩‍🔬", ...args);
 }
