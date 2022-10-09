40c1cb8a72fb0a74abdf16d6f8f7d7e2cf611f13
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-10-09 17:23:57 +0200

    feat(ui): Icons for enable/disable all buttons

--- packages/userscript/source/ui/components/SettingsList.ts
+++ packages/userscript/source/ui/components/SettingsList.ts
@@ -23,8 +23,11 @@ export class SettingsList extends UiComponent {
     const containerList = $("<ul/>").addClass("ks-list").addClass("ks-items-list");
 
     const disableAllButton = $("<div/>")
-      .text(host.engine.i18n("ui.disable.all"))
-      .addClass("ks-button");
+      .html(
+        '<svg style="width: 15px; height: 15px;" viewBox="0 0 48 48"><path fill="currentColor" d="M9 42q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6h30q1.2 0 2.1.9.9.9.9 2.1v30q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h30V9H9v30Z" /></svg>'
+      )
+      .prop("title", host.engine.i18n("ui.disable.all"))
+      .addClass("ks-icon-button");
 
     disableAllButton.on("click", function () {
       // can't use find as we only want one layer of checkboxes
@@ -37,8 +40,11 @@ export class SettingsList extends UiComponent {
     containerList.append(disableAllButton);
 
     const enableAllButton = $("<div/>")
-      .text(host.engine.i18n("ui.enable.all"))
-      .addClass("ks-button")
+      .html(
+        '<svg style="width: 15px; height: 15px;" viewBox="0 0 48 48"><path fill="currentColor" d="M20.95 31.95 35.4 17.5l-2.15-2.15-12.3 12.3L15 21.7l-2.15 2.15ZM9 42q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6h30q1.2 0 2.1.9.9.9.9 2.1v30q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h30V9H9v30ZM9 9v30V9Z" /></svg>'
+      )
+      .prop("title", host.engine.i18n("ui.enable.all"))
+      .addClass("ks-icon-button")
       .addClass("ks-margin-right");
 
     enableAllButton.on("click", function () {
