04562030092f7720d800cc4d65c27187b383a452
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-11-07 19:48:25 +0100

    ci: Try running on BrowserStack

---
+++ packages/tests/source/smoke.spec.ts
@@ -0,0 +1,46 @@
+import "chromedriver";
+import { Builder, WebDriver } from "selenium-webdriver";
+import config from "./config";
+import { KittenGamePage } from "./pages/KittenGamePage";
+
+const username = process.env.BROWSERSTACK_USERNAME;
+const accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
+
+describe("Smoke test", function () {
+  let driver: WebDriver;
+  let kg: KittenGamePage;
+
+  before(async function () {
+    if (username && accessKey) {
+      driver = await new Builder()
+        .usingServer(`http://${username}:${accessKey}@hub.browserstack.com/wd/hub`)
+        .withCapabilities({
+          "bstack:options": {
+            os: "Windows",
+            osVersion: "11",
+          },
+          browserName: "Chrome",
+          browserVersion: "103.0",
+        })
+        .build();
+    } else {
+      driver = await new Builder().forBrowser("chrome").build();
+    }
+
+    await driver.navigate().to(config.baseUrl);
+  });
+
+  beforeEach(async function () {
+    kg = new KittenGamePage(driver);
+
+    await kg.waitForLoadComplete();
+    await kg.wipe();
+    await kg.waitForLoadComplete();
+  });
+
+  after(async () => await driver.quit());
+
+  it("Gathers catnip", async function () {
+    await kg.testGatherCatnip();
+  });
+});
