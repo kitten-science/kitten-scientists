3add12a3c8a2b336f53ce2728482c10f64076331
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-12-25 16:44:44 +0100

    chore: Prepare documentation

--- packages/documentation/.scripts/serve.sh
+++ packages/documentation/.scripts/serve.sh
@@ -7,4 +7,4 @@ cd ${basedir}/..
 docker build -t mkdocs-material .
 
 cd ../..
-docker run --rm -it -p 8000:8000 -v ${PWD}:/docs mkdocs-material serve --config-file packages/documentation/mkdocs.yml
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs ghcr.io/oliversalzburg/mkdocs-material-ex:main serve --config-file packages/documentation/mkdocs.yml
