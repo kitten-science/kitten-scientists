b6cc621727db00b4f762805f2121409bd4ff5538
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-12-28 17:57:56 +0100

    fix(ui): Excessive hover guides
    Hover guides appeared on headers and explainers, which isn't helpful.

--- packages/userscript/source/ui/UserInterface.ts
+++ packages/userscript/source/ui/UserInterface.ts
@@ -166,7 +166,7 @@ export class UserInterface {
     );
     // Hover guides
     this._addRule(
-      `#ks ul li:not(.ks-expanded):hover { 
+      `#ks ul .ks-setting:not(.ks-expanded):hover { 
         border-bottom: 1px solid rgba(185, 185, 185, 0.5);
       }`
     );
