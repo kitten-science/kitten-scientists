e7b1f9263d611b2d4b2f18fb0284dd07dbcad58e
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-01-09 22:45:26 +0100

    refactor(ui): Improve nesting buttons in lists

--- packages/userscript/source/ui/components/ButtonListItem.ts
+++ packages/userscript/source/ui/components/ButtonListItem.ts
@@ -1,27 +1,20 @@
 import { UserScript } from "../../UserScript";
-import { ListItem } from "./ListItem";
+import { ListItem, ListItemOptions } from "./ListItem";
 import { TextButton } from "./TextButton";
-import { UiComponent } from "./UiComponent";
 
-export class ButtonListItem extends UiComponent implements ListItem {
-  readonly element: JQuery<HTMLElement>;
+export class ButtonListItem extends ListItem {
   readonly button: TextButton;
 
-  get elementLabel() {
-    return this.button.element;
+  constructor(host: UserScript, button: TextButton, options?: Partial<ListItemOptions>) {
+    super(host, options);
+
+    this.button = button;
+    this.element.append(button.element);
   }
 
-  constructor(host: UserScript, button: TextButton, delimiter = false) {
-    super(host);
+  refreshUi(): void {
+    super.refreshUi();
 
-    const element = $(`<li/>`);
-    for (const cssClass of ["ks-setting", delimiter ? "ks-delimiter" : ""]) {
-      element.addClass(cssClass);
-    }
-
-    element.append(button.element);
-
-    this.element = element;
-    this.button = button;
+    this.button.refreshUi();
   }
 }
