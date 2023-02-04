c13585e0c4fc51cd7040aa8468792821a6230e26
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-02-04 17:59:18 +0100

    refactor: Split different build outputs

--- packages/tests/.mocharc.cjs
+++ packages/tests/.mocharc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  require: "output/tests/hooks.js",
+  require: "./build/tests/hooks.js",
   // 1000 milliseconds = 1 second * 60 = 1 minute * 5 = 5 minutes
   timeout: 1000 * 60 * 5,
 };
