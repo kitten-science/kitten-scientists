598c9decc510d88b12e4d42221ce59605b63d642
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-10-24 18:31:33 +0200

    ci: Integrate `action-automatic-releases`
    See: https://github.com/marvinpinto/action-automatic-releases
    See: https://github.com/marvinpinto/actions
    
    Action is unmaintained, but we like it.

---
+++ @types/conventional-changlog-angular/index.d.ts
@@ -0,0 +1,4 @@
+declare module "conventional-changelog-angular/conventional-recommended-bump" {
+  declare const defaultChangelogOpts: Record<string, unknown>;
+  export default defaultChangelogOpts;
+}
