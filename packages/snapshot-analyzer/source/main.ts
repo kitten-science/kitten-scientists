5a555d7e8b05ac6a82c82e3ff2fac862ff24587a
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-01-05 19:01:13 +0100

    feat: Validation

--- packages/snapshot-analyzer/source/main.ts
+++ packages/snapshot-analyzer/source/main.ts
@@ -1,12 +1,25 @@
 import { UserScript } from "@kitten-science/kitten-scientists";
 import { getDocumentElementTypeByIdStrict } from "@oliversalzburg/js-utils/dom/core.js";
+import SlInput from "@shoelace-style/shoelace/dist/components/input/input.js";
+import SlTextarea from "@shoelace-style/shoelace/dist/components/textarea/textarea.js";
 import { decompressFromBase64 } from "lz-string";
 
-const input = getDocumentElementTypeByIdStrict(document, "input", HTMLInputElement);
-const parsed = getDocumentElementTypeByIdStrict(document, "parsed", HTMLTextAreaElement);
+const input = getDocumentElementTypeByIdStrict(document, "input", SlInput);
+const parsed = getDocumentElementTypeByIdStrict(document, "parsed", SlTextarea);
 
-input.addEventListener("change", (event: Event) => {
-  // Pre-decompress
-  const decompressed = decompressFromBase64(input.value);
-  parsed.value = JSON.stringify(UserScript.decodeSettings(decompressed), undefined, 2);
+await Promise.all([
+  customElements.whenDefined("sl-input"),
+  customElements.whenDefined("sl-textarea"),
+]);
+
+input.addEventListener("sl-change", (event: Event) => {
+  try {
+    // Pre-decompress
+    const decompressed = decompressFromBase64(input.value);
+    parsed.value = JSON.stringify(UserScript.decodeSettings(decompressed), undefined, 2);
+    input.setCustomValidity("");
+  } catch (error) {
+    console.debug(error);
+    input.setCustomValidity("Invalid KS state");
+  }
 });
