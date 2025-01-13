abe4a0ea1f6a1691195038a8946c5f6ac616bc7a
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-01-13 21:31:57 +0100

    chore: Format `.css` files on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 /** @type {import("lint-staged").Config} */
 export default {
   "package.json": "yarn prettier-package-json --write",
-  "*.{js,json,md,sh,ts,yml}": "prettier --write",
+  "*.{css,js,json,md,sh,ts,yml}": "prettier --write",
 };
