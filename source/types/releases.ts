ed0e506948690765dfc36e1397ec2389c21b08aa
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-02-28 11:13:33 +0100

    fix: Import from action

---
+++ source/types/releases.ts
@@ -0,0 +1,32 @@
+export type ReleaseMeta = {
+  /**
+   * The version string of the release.
+   */
+  version: string;
+
+  /**
+   * When was this release created?
+   */
+  date: string;
+
+  url: {
+    /**
+     * A URL that points to the userscript.
+     */
+    default: string;
+
+    /**
+     * The same userscript, but minified.
+     */
+    minified: string;
+
+    /**
+     * Points to the GitHub release for this version.
+     */
+    release: string;
+  };
+};
+
+export type TravelingReleaseChannel = "dev" | "nightly" | "stable";
+export type ReleaseChannel = "fixed" | TravelingReleaseChannel;
+export type ReleaseInfoSchema = Record<TravelingReleaseChannel, ReleaseMeta>;
