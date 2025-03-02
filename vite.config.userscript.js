578fd24213a729e44222e7fba96065133c4d576b
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-03-02 15:45:40 +0100

    ci: Clean up build, pipeline

--- vite.config.userscript.js
+++ vite.config.userscript.js
@@ -4,30 +4,8 @@ import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { metablock } from "vite-plugin-userscript";
 import manifest from "./package.json" with { type: "json" };
 
-const isCi = Boolean(process.env.CI);
-const isDevBuild = String(process.env.DEV_BUILD) === "true";
-const isNightlyBuild = String(process.env.NIGHTLY_BUILD) === "true";
 const minify = Boolean(process.env.MINIFY) ?? !isDevBuild;
-
-function getDateString() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = `${date.getMonth() + 1}`.padStart(2, "0");
-  const day = `${date.getDate()}`.padStart(2, "0");
-  return `${year}${month}${day}`;
-}
-
-const versionString = [
-  manifest.version,
-  isDevBuild ? "-dev" : "",
-  isNightlyBuild ? `-${getDateString()}` : "",
-  (isDevBuild || isNightlyBuild) && process.env.GITHUB_SHA
-    ? `-${String(process.env.GITHUB_SHA).substring(0, 7)}`
-    : "",
-].join("");
-const updateURL = `https://kitten-science.com/${
-  isNightlyBuild ? "nightly.js" : isDevBuild ? "dev.js" : "stable.js"
-}`;
+const versionString = process.env.RELEASE_VERSION ?? "0.0.0-ci";
 
 const filename = ["kitten-scientists", `-${versionString}`, minify ? ".min" : "", ".user.js"].join(
   "",
@@ -35,6 +13,8 @@ const filename = ["kitten-scientists", `-${versionString}`, minify ? ".min" : ""
 
 const KS_RELEASE_CHANNEL = JSON.stringify(process.env.KS_RELEASE_CHANNEL ?? "fixed");
 const KS_VERSION = JSON.stringify(versionString);
+
+const updateURL = `https://kitten-science.com/${KS_RELEASE_CHANNEL}.js`;
 
 export default defineConfig({
   build: {
