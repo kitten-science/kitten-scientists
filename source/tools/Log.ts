3b6ec80d40dddc9e50c079c825725060022c66f2
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-05-12 11:39:20 +0200

    feat(core): Log error stack traces

--- source/tools/Log.ts
+++ source/tools/Log.ts
@@ -1,4 +1,14 @@
+import { is, type Maybe } from "@oliversalzburg/js-utils/data/nil.js";
+
 export const cl = <T extends Array<unknown>>(...args: T): Array<string | T[number]> => [
   "👩‍🔬",
-  ...args.filter(arg => arg !== ""),
+  ...args
+    .filter(arg => arg !== "")
+    .flatMap((maybeError: Maybe<Error> | unknown) => {
+      if (is(maybeError, Error)) {
+        const error = maybeError;
+        return [error.name, error.message, error.stack];
+      }
+      return maybeError;
+    }),
 ];
