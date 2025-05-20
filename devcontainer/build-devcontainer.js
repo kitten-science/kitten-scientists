5304ace262b55b87e7366b9f377c9513c438525a
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-05-20 14:16:25 +0200

    fix(container): Entrypoint not marked executable

--- devcontainer/build-devcontainer.js
+++ devcontainer/build-devcontainer.js
@@ -1,3 +1,4 @@
+import { chmod } from "node:fs/promises";
 import { redirectErrorsToConsole } from "@oliversalzburg/js-utils/errors/console.js";
 import esbuild from "esbuild";
 
@@ -12,4 +13,5 @@ esbuild
     platform: "node",
     target: "node22",
   })
+  .then(() => chmod("output/devcontainer/entrypoint-devcontainer.mjs", 0o775))
   .catch(redirectErrorsToConsole(console));
