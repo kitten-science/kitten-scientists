f11b46df24ba464f21cb30d401aa6fc84b25e092
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-05-24 01:24:18 +0200

    fix(ui): Excessive DOM manipulation for stock indicator

--- source/WorkshopManager.ts
+++ source/WorkshopManager.ts
@@ -679,27 +679,44 @@ export class WorkshopManager extends UpgradeManager implements Automation {
    * The user can configure this in the Workshop automation section.
    */
   refreshStock() {
+    const resourceCells = [
+      ...document.querySelectorAll(
+        "#game .res-row .res-cell.resAmount, #game .res-row .res-cell.resource-value",
+      ),
+    ];
+    const cellMap = new Map(
+      resourceCells.map(_ => [
+        mustExist(
+          [...(_.parentNode as HTMLElement).classList.entries()].find(([_i, __]) =>
+            __.startsWith("resource_"),
+          ),
+        )[1].substring(9) as Resource,
+        _,
+      ]),
+    );
+
     for (const [name, resource] of objectEntries(this._host.engine.settings.resources.resources)) {
-      const resourceCells = [
-        // Resource table on the top.
-        ...$(`#game .res-row.resource_${name} .res-cell.resAmount`),
-        // Craft table on the bottom.
-        ...$(`#game .res-row.resource_${name} .res-cell.resource-value`),
-      ];
+      const cell = cellMap.get(name);
+
+      // For some configured resources, there might be no UI cell.
+      if (!cell) {
+        continue;
+      }
 
       if (!resource.enabled || resource.stock === 0) {
-        for (const resourceCell of resourceCells) {
-          resourceCell.classList.remove("ks-stock-above", "ks-stock-below");
+        if (
+          cell.classList.contains("ks-stock-above") ||
+          cell.classList.contains("ks-stock-below")
+        ) {
+          cell.classList.remove("ks-stock-above", "ks-stock-below");
         }
         continue;
       }
 
       const isBelow = this._host.game.resPool.get(name).value < resource.stock;
 
-      for (const resourceCell of resourceCells) {
-        resourceCell.classList.add(isBelow ? "ks-stock-below" : "ks-stock-above");
-        resourceCell.classList.remove(isBelow ? "ks-stock-above" : "ks-stock-below");
-      }
+      cell.classList.add(isBelow ? "ks-stock-below" : "ks-stock-above");
+      cell.classList.remove(isBelow ? "ks-stock-above" : "ks-stock-below");
     }
   }
 }
