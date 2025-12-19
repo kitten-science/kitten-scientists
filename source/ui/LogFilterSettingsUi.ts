5c94ef6faf74d482c9b762288a19c861d0efc763
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-12-19 11:25:58 +0100

    feat(core): Set game log filters from KS

--- source/ui/LogFilterSettingsUi.ts
+++ source/ui/LogFilterSettingsUi.ts
@@ -1,7 +1,12 @@
-import { FilterItems, type LogFilterSettings } from "../settings/LogFilterSettings.js";
+import {
+  FilterItems,
+  FilterItemsGame,
+  type LogFilterSettings,
+} from "../settings/LogFilterSettings.js";
 import { cl } from "../tools/Log.js";
 import { Container } from "./components/Container.js";
 import { ExplainerListItem } from "./components/ExplainerListItem.js";
+import { HeaderListItem } from "./components/HeaderListItem.js";
 import stylesLabelListItem from "./components/LabelListItem.module.css";
 import { SettingListItem } from "./components/SettingListItem.js";
 import { SettingsList } from "./components/SettingsList.js";
@@ -31,28 +36,52 @@ export class LogFiltersSettingsUi extends SettingsPanel<LogFilterSettings> {
         hasDisableAll: false,
         hasEnableAll: false,
       }).addChildren([
-        new SettingListItem(this, settings.disableKGLog, this.host.engine.i18n("filter.allKG")),
+        new HeaderListItem(this, "Kitten Scientists"),
+        new SettingsList(this).addChildren(
+          FilterItems.map(item => {
+            return { label: this.host.engine.i18n(`filter.${item}`), name: item };
+          })
+            .sort((a, b) => a.label.localeCompare(b.label))
+            .map(
+              item =>
+                new SettingListItem(this, this.setting.filters[item.name], item.label, {
+                  onCheck: () => {
+                    this.host.engine.imessage("filter.enable", [item.label]);
+                  },
+                  onUnCheck: () => {
+                    this.host.engine.imessage("filter.disable", [item.label]);
+                  },
+                }),
+            ),
+        ),
       ]),
     );
 
-    const listFilters = new SettingsList(this).addChildren(
-      FilterItems.map(item => {
-        return { label: this.host.engine.i18n(`filter.${item}`), name: item };
-      })
-        .sort((a, b) => a.label.localeCompare(b.label))
-        .map(
-          item =>
-            new SettingListItem(this, this.setting.filters[item.name], item.label, {
-              onCheck: () => {
-                this.host.engine.imessage("filter.enable", [item.label]);
-              },
-              onUnCheck: () => {
-                this.host.engine.imessage("filter.disable", [item.label]);
-              },
-            }),
+    this.addChildContent(
+      new SettingsList(this, {
+        hasDisableAll: false,
+        hasEnableAll: false,
+      }).addChildren([
+        new HeaderListItem(this, "Kittens Game"),
+        new SettingsList(this).addChildren(
+          FilterItemsGame.map(item => {
+            return { label: this.host.engine.i18n(`$console.filter.${item}`), name: item };
+          })
+            .sort((a, b) => a.label.localeCompare(b.label))
+            .map(
+              item =>
+                new SettingListItem(this, this.setting.filtersGame[item.name], item.label, {
+                  onCheck: () => {
+                    this.host.engine.imessage("filter.enable", [item.label]);
+                  },
+                  onUnCheck: () => {
+                    this.host.engine.imessage("filter.disable", [item.label]);
+                  },
+                }),
+            ),
         ),
+      ]),
     );
-    this.addChildContent(listFilters);
 
     this.addChildContent(new ExplainerListItem(this, "filter.explainer"));
   }
